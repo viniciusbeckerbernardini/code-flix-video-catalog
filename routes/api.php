c1d5ad45779fc70fe28470818c2c5f712439f213
viniciusbeckerbernardini <vbkmma@gmail.com>
2024-01-18 09:09:59 -0300

    Pushing initial files. Categories resource created

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +15,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace'=>'Api'], function (){
+    Route::resource('categories','CategoryController', ['except'=>['create','edit']]);
+});
+
