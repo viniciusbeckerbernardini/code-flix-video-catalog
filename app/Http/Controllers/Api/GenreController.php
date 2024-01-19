ecbb24910ca7ac3b278c617343d14549089f166f
viniciusbeckerbernardini <vbkmma@gmail.com>
2024-01-19 07:42:34 -0300

    Adding genre resource

---
+++ app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
+    public function index(Request $request)
+    {
+        if($request->has('only_trashed')){
+            return Category::onlyTrashed()->get();
+        }
+
+        return Genre::all();
+    }
+
+
+    public function store(Request $request)
+    {
+        $this->validate($request,$this->rules);
+
+        return Genre::create($request->all());
+    }
+
+    public function show(Genre $genre)
+    {
+        return $genre;
+    }
+
+    public function update(Request $request, Genre $genre)
+    {
+        $this->validate($request,$this->rules);
+        $genre->update($request->all());
+        return $genre;
+    }
+
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+        return response()->noContent();
+    }
+}
